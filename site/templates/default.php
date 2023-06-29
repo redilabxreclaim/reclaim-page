ff176c6358095d1a3ed4bcbbd1b410f1e72d9b0c
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2023-06-29 13:47:31 +0200

    navbar initial design for the home page created

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $site->title() ?></title>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;700&display=swap" rel="stylesheet">
+    <?= css('../assets/css/index.css') ?>
+</head>
+<body>
+<header class='header'>
+   <div class='logo-div'>
+      <a class='org' href="<?= $site->url() ?>"><h1><?= $site->title() ?></h1></a>
+      <a class='org' href="<?= $site->url() ?>"><p>ecofeminist alliance</p></a>
+</div>
+        <nav class='menu'>
+            <ul class='linkss'>
+            <?php foreach ($site->children()->listed() as $item): ?>
+                <li><a class='header-button' href="<?= $item->url() ?> "><?= $item->title() ?></a></li>
+            <?php endforeach ?>
+            </ul>
+        </nav>
+    </header>      
 <h1><?= $page->title() ?></h1>
+    <?= $page->text() ?>
+</body>
+</html>
