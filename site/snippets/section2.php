548aaf14b4d30a4798c1aa5fcaaad05c70505965
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-07-07 11:51:42 +0200

    create basic strcuture for section2

---
+++ site/snippets/section2.php
@@ -0,0 +1,16 @@
+<section class="section2">
+  <div class="video-background">
+
+  <video width="560" height="315" autoplay loop muted >
+        <source src="<?= url('media/Festival.mp4') ?>" type="video/mp4">
+      </video>
+
+  
+  <div class="content">
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+    <p>Paragraph 3</p>
+    
+    <button>Button</button>
+  </div>
+</section>
