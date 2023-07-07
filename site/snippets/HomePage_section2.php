a0c2ed2024d0e699167ab7a7a2e5c0b9abad9bd3
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-07-07 20:14:48 +0200

    add styling to section 2

---
+++ site/snippets/HomePage_section2.php
@@ -0,0 +1,26 @@
+<?= css('../assets/css/homePageSec2.css') ?>
+
+
+
+
+<section class="section2">
+
+    <div class="video-background">
+
+        <video autoplay loop muted>
+            <source src="<?= url('media/Festival.mp4') ?>" type="video/mp4">
+        </video>
+
+
+    </div>
+    <div class="content">
+        <div></div>
+        <p class="p1">BERLIN’S FIRST ECOFEMINIST FESTIVAL</p>
+        <p class="p2">Seeding Freedom</p>
+        <p class="p3">Coming back in 2024 <span>Berlin</span></p>
+
+    </div>
+    <button class="learnBtn">LEARN MORE</button>
+
+    </div>
+</section>
