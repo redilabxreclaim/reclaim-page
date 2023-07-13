eaccb9e1ef462d04c3a282a9ba2474583fd012e4
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-07-13 15:49:21 +0200

    add images files

---
+++ site/snippets/HomePage_section4.php
@@ -0,0 +1,26 @@
+<?= css('../assets/css/homePage_section4.css') ?>
+
+
+
+
+<section class="section4">
+
+
+    <div class="content">
+
+        <p class='p1'>
+            MEET THE TEAM
+        </p>
+        <p class='p2'>We are eight friends brought together by our shared vision of an ecofeminist world.
+        </p>
+
+        <div>
+            <img src="<?= $page->image('reclaim_team.png')->url() ?>" alt="">
+
+        </div>
+
+
+        <button class="learnBtn">LEARN MORE</button>
+
+    </div>
+</section>
