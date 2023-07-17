5fcc9011c0c0d5408ce22bbe8455682169410b20
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-07-17 16:05:08 +0200

    change naming of calsses

--- site/snippets/HomePage_section4.php
+++ site/snippets/HomePage_section4.php
@@ -3,23 +3,23 @@
 
 
 
-<section class="section4">
+<section class="HM_section4">
 
 
-    <div class="content">
+    <div class="HM_4_content">
 
-        <p class='p1'>
+        <p class='HM_4_p1'>
             MEET THE TEAM
         </p>
-        <p class='p2'>We are eight friends brought together by our shared vision of an ecofeminist world.
+        <p class='HM_4_p2'>We are eight friends brought together by our shared vision of an ecofeminist world.
         </p>
 
 
 
 
-        <img class="teamImg" src="<?= url('HomePage_section4/reclaim_team.png') ?>" alt="">
-        <img class="ImgBG" src="<?= url('HomePage_section4/Circle.svg') ?>" alt="">
+        <img class="HM_4_teamImg" src="<?= url('HomePage_section4/reclaim_team.png') ?>" alt="">
+        <img class="HM_4_ImgBG" src="<?= url('HomePage_section4/Circle.svg') ?>" alt="">
 
-    <button class="learnBtn">LEARN MORE</button>
+        <button class="HM_4_learnBtn">LEARN MORE</button>
     </div>
 </section>
