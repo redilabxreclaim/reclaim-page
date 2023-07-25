56be1ef8b2832f2602dda69421573a47b6343b77
SalazarEmiliano <salazaremiliano.dev@gmail.com>
2023-07-25 19:38:25 +0200

    Added bold to Reclaim word

--- site/snippets/HomePage_section1.php
+++ site/snippets/HomePage_section1.php
@@ -1,7 +1,7 @@
 <?= css('../assets/css/HomePage_section1.css') ?>
 
 <div class="section-one">
-        <h1 class="main-title"><?= $page->title() ?></h1>
+        <h1 class="main-title"><?= $page->title() ?> <span class="bold-text"><?= $page->titleappend() ?></span></h1>
         <img class="reclaim-logo" src="<?= $page->image('reclaim-logo.png')->url() ?>" alt="Reclaim Logo">
         <div class="paragraph">
             <p><?= $page->text() ?></p>    
