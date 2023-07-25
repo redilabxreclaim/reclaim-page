46dd3fb0322ef31cdfeadffd83318dea887309fd
SalazarEmiliano <salazaremiliano.dev@gmail.com>
2023-07-25 16:40:20 +0200

    Styles for section 1 moved to HomePage_section1.css

--- site/snippets/HomePage_section1.php
+++ site/snippets/HomePage_section1.php
@@ -1,3 +1,5 @@
+<?= css('../assets/css/HomePage_section1.css') ?>
+
 <div class="section-one">
         <h1 class="main-title"><?= $page->title() ?></h1>
         <img class="reclaim-logo" src="<?= $page->image('reclaim-logo.png')->url() ?>" alt="Reclaim Logo">
