14c6435454cd2ddedb2feb0ab7f14b4772341889
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-08-01 16:21:22 +0200

    section1 in html css

--- site/snippets/aboutPage/aboutPage_section1.php
+++ site/snippets/aboutPage/aboutPage_section1.php
@@ -1,5 +1,22 @@
 <?= css('../assets/css/aboutPageCss/aboutPage_section1.css') ?>
 
 
+<section class="about_section1">
+    <section class="about_section1">
+        <?php if($image = $page->image('EllipseAboutSection1.png')): ?>
+        <img class="elop_img_about_sec1" src="<?= $image->url() ?>" alt="">
+        <?php endif ?>
 
-<h1 class="hi">hi</h1>
+        <div class="rectangle_about_sec1">
+
+        </div>
+
+        <p class="about_sec1_p1">
+            <?= $page->section1_text1() ?>
+
+        </p>
+    </section>
+
+
+
+</section>
