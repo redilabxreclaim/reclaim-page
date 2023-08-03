31d6ba45d154314f4fcfb575b4a298dfdddb5121
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-08-03 17:25:20 +0200

    add simple html strucutre

--- site/snippets/projectsPage/oldProjects.php
+++ site/snippets/projectsPage/oldProjects.php
@@ -1,3 +1,41 @@
+<?= css('../assets/css/projectsPage/oldProjects.css') ?>
 
 
 
+
+
+<?php
+// /site/snippets/two-column-layout.php
+
+// Default values
+$title = $title ?? 'Default title';
+$headline = $text ?? 'Default text';
+$button = $button ?? 'Default button';
+$img = $img ?? 'Default image path';
+$order = $order ?? 'normal'; // normal or reversed
+
+?>
+
+<div class="ProjectPage-oldProjects" style=" flex-direction: <?= $order === 'normal' ? 'row' : 'row-reverse' ?>;">
+    <div class="text-container">
+
+        <div class="projectsTitle">
+            <p>RECLAIM</p>
+            <p><?= $title ?></p>
+        </div>
+
+        <div>
+            <p><?= $text ?></p>
+
+        </div>
+
+        <div>
+            <p><?= $text ?></p>
+
+        </div>
+        <button><?= $button ?></button>
+    </div>
+    <div class="img-container">
+        <img src="<?= $img ?>" alt="">
+    </div>
+</div>
