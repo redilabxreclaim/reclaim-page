1476c120f0769955d32759a45941caddffdad1da
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-08-03 17:56:11 +0200

    add title style

--- site/snippets/projectsPage/oldProjects.php
+++ site/snippets/projectsPage/oldProjects.php
@@ -16,26 +16,31 @@ $order = $order ?? 'normal'; // normal or reversed
 
 ?>
 
-<div class="ProjectPage-oldProjects" style=" flex-direction: <?= $order === 'normal' ? 'row' : 'row-reverse' ?>;">
-    <div class="text-container">
+<div class="ProjectPage-oldProjects">
 
-        <div class="projectsTitle">
-            <p>RECLAIM</p>
-            <p><?= $title ?></p>
-        </div>
-
-        <div>
-            <p><?= $text ?></p>
-
-        </div>
-
-        <div>
-            <p><?= $text ?></p>
-
-        </div>
-        <button><?= $button ?></button>
+    <div class="oldprojects_title">
+        <p class="oldprojects_title_part1">RECLAIM</p>
+        <p class="oldprojects_title_part2"><?= $title ?></p>
     </div>
-    <div class="img-container">
-        <img src="<?= $img ?>" alt="">
+
+    <div class="oldprojects_container" style="flex-direction: <?= $order === 'normal' ? 'row' : 'row-reverse' ?>;">
+
+        <div class="oldprojects_container1">
+
+
+            <div>
+                <p><?= $headline ?></p>
+
+            </div>
+
+            <div>
+                <p><?= $text ?></p>
+
+            </div>
+            <button><?= $button ?></button>
+        </div>
+        <div class="oldprojects_img">
+            <img src="<?= $img ?>" alt="">
+        </div>
     </div>
 </div>
