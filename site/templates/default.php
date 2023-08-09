cfcb3db3019e3140903c2c130f76850f7e2ab068
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-08-09 14:49:46 +0200

    add emilano work to defult page

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,15 +9,22 @@
     <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;700&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&family=Barlow:wght@400;700&family=Overpass:wght@100&family=Roboto:wght@400;700&family=Rubik:ital@1&display=swap" rel="stylesheet">
-<?= css('../assets/css/index.css') ?>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&family=Barlow:wght@400;700&family=Overpass:wght@100&family=Roboto:wght@400;700&family=Rubik:ital@1&display=swap"
+        rel="stylesheet">
+    <?= css('../assets/css/index.css') ?>
 </head>
+
 <body>
-   <header class='header'>
-   <div class='logo-div'>
-      <a class='orgHome' href="<?= $site->url() ?>"><h1><?= $site->title() ?></h1></a>
-      <a class='orgHome' href="<?= $site->url() ?>"><p>Ecofeminist Alliance</p></a>
-   </div>
+    <header class='header'>
+        <div class='logo-div'>
+            <a class='orgHome' href="<?= $site->url() ?>">
+                <h1><?= $site->title() ?></h1>
+            </a>
+            <a class='orgHome' href="<?= $site->url() ?>">
+                <p>Ecofeminist Alliance</p>
+            </a>
+        </div>
         <nav class='menu'>
             <ul class='menu-links'>
                 <?php foreach ($site->children()->listed() as $item): ?>
@@ -24,17 +32,12 @@
                 <?php endforeach ?>
             </ul>
         </nav>
-  </header>
-    <?php snippet('HomePage_section5') ?>
-    <div class="section-one">
-        <h1 class="main-title"><?= $page->title() ?></h1>
-        <img class="reclaim-logo" src="<?= $page->image('reclaim-logo.png')->url() ?>" alt="Reclaim Logo">
-        <div class="paragraph">
-            <p><?= $page->text() ?></p>    
-        </div>
-        <button class="learn-more">LEARN MORE</button>
-    </div>      
-    <?php snippet('HomePage_section4') ?>
+    </header>
+    <?php snippet('HomePage_section1') ?>
     <?php snippet('HomePage_section2') ?>
+    <?php snippet('HomePage_section3') ?>
+    <?php snippet('HomePage_section4') ?>
+    <?php snippet('HomePage_section5') ?>
 </body>
+
 </html>
