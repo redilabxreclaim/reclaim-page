c93a915730354802ddee657fcd301ea2672bae29
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-08-18 18:25:45 +0200

    adjust section 3 styling

--- site/snippets/aboutPage/aboutPage_section3.php
+++ site/snippets/aboutPage/aboutPage_section3.php
@@ -5,31 +5,34 @@
 <section class="about_section3">
 
 
-    <div class="whoweare_img_about_sec2_container">
+    <div class="whoweare_img_about_sec3_container">
 
-        <?php if($image = $page->image('Section 2 Picture Who we are.png')): ?>
-        <img class="whoweare_img_about_sec2" src="<?= $image->url() ?>" alt="">
+        <?php if($image = $page->image('Section3What.svg')): ?>
+        <img class="img_about_sec32" src="<?= $image->url() ?>" alt="">
+        <?php endif ?>
+        <?php if($image = $page->image('Section3Picture.JPG')): ?>
+        <img class="img_about_sec3" src="<?= $image->url() ?>" alt="">
         <?php endif ?>
     </div>
 
-    <p class="about_section2_title">
+    <p class="about_section3_title">
 
         <?= $page->section3_title() ?>
 
     </p>
-    <article class="about_section2_text">
+    <article class="about_section3_text">
 
 
-        <p class="about_section2_p_normal">
+        <p class="about_section3_p_normal">
             <?= $page->section3_text11()->kt()->inline() ?>
 
         </p>
 
-        <p class="about_section2_p_normal">
+        <p class="about_section3_p_normal">
             <?= $page->section3_text2()->kt()->inline() ?>
 
         </p>
-        <p class="about_section2_p_normal">
+        <p class="about_section3_p_normal">
             <?= $page->section3_text3()->kt()->inline() ?>
 
         </p>
