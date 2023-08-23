785270102e05b4869f1bdac96cae19841f213b8d
SalazarEmiliano <salazaremiliano.dev@gmail.com>
2023-08-24 00:17:59 +0200

    Youtube video link added to Learn More btn

--- site/snippets/HomePage_section2.php
+++ site/snippets/HomePage_section2.php
@@ -6,24 +6,25 @@
 <section class="section2">
 
     <div class="video-background">
-
         <video autoplay loop muted>
             <source src="<?= url('home/media/Festival.mp4') ?>" type="video/mp4">
         </video>
-
-
     </div>
-    <div class="content">
-        <div></div>
+    <div class="content">        
         <p class="p1"> <?= $page->section2_text1() ?>
         </p>
         <p class="p2"> <?= $page-> section2_text2()?></p>
-        <p class="p3"><?= $page-> section2_text3()?><span><?= $page-> section2_text4()?></span></p>
-
+        <p class="p3"><?= $page-> section2_text3()?><span><?= $page-> section2_text4()?></span></p>    
+        
     </div>
-    <button class="learnBtn"><?= $page-> section2_text5()?></button>
-
-    </div>
+   
+        <a href="https://www.youtube.com/watch?v=S4mVbO1B6mg" target="_blank" >
+            <button class="learnBtn"><?= $page-> section2_text5()?></button>
+        </a>
+    
+   
+    
+    
 </section>
 
 <!-- test -->
