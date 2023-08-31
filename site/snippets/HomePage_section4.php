96b9491666923dc58207348978a31adbd11f0513
SalazarEmiliano <salazaremiliano.dev@gmail.com>
2023-08-31 16:13:34 +0200

    Links Learn More section 4 and Let's chat updated

--- site/snippets/HomePage_section4.php
+++ site/snippets/HomePage_section4.php
@@ -24,7 +24,7 @@
         <img class="HM_4_teamImg" src="<?= url('home/HomePage_section4/reclaim_team.png') ?>" alt="">
         <img class="HM_4_ImgBG" src="<?= url('home/HomePage_section4/Circle.svg') ?>" alt="">
 
-        <a class="HM_4_learna" href="<?= $kirby->url('') ?>/about#learn-more">
+        <a class="HM_4_learna" href="about">
             <button class="HM_4_learnBtn">
                 <?= $page->section4_text3() ?>
                 <!-- LEARN MORE -->
