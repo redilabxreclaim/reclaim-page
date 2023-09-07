ae6b96ecc7df96a394475cc0aeeb19e3d1b55709
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-09-07 16:26:28 +0200

    home sec 2 res

--- site/snippets/HomePage_section2.php
+++ site/snippets/HomePage_section2.php
@@ -9,12 +9,11 @@
 
         <div class="video-background">
             <video autoplay loop muted>
-                <source src="<?= url('home/media/Festival.mp4') ?>" type="video/mp4">
+                <source class="played-video" src="<?= url('home/media/Festival.mp4') ?>" type="video/mp4">
             </video>
         </div>
         <div class="content">
-            <p class="p1"> <?= $page->section2_text1() ?>
-            </p>
+            <p class="p1"> <?= $page->section2_text1() ?> </p>
             <p class="p2"> <?= $page-> section2_text2()?></p>
             <p class="p3"><?= $page-> section2_text3()?><span><?= $page-> section2_text4()?></span></p>
 
