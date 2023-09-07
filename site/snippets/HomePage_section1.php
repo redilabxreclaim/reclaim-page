951958ec172e95684990a0350a402e304a617977
Ahmed Amin <ahmed.amin.abd.elwadod@gmail.com>
2023-09-07 13:47:34 +0200

    adjust home page section 1

--- site/snippets/HomePage_section1.php
+++ site/snippets/HomePage_section1.php
@@ -1,12 +1,15 @@
 <?= css('../assets/css/HomePage_section1.css') ?>
 
-<div class="section-one">
+<div class="home-section-one_container">
+    <div class="home-section-one">
+
         <h1 class="main-title"><?= $page->title() ?> <span class="bold-text"><?= $page->title2() ?></span></h1>
-        <img class="reclaim-logo" src="<?= $page->image('reclaim-logo.png')->url() ?>" alt="Reclaim Logo">
-        <div class="paragraph">
-            <p><?= $page->text() ?></p>    
+        <img class="home-reclaim-logo" src="<?= $page->image('reclaim-logo.png')->url() ?>" alt="Reclaim Logo">
+        <div class="Home-page-sec1-paragraph">
+            <p><?= $page->text() ?></p>
         </div>
-        <a href="about#about-section-one">
-            <button class="learn-more">LEARN MORE</button>
+        <a href="about#about-section-one" class="Home-page-sec1-learn-more-a">
+            <button class="Home-page-sec1-learn-more">LEARN MORE</button>
         </a>
-</div>  
+    </div>
+</div>
